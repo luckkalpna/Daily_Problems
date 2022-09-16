dfee7fd3767820ed22c43c5ef89b1f2ff877dbd3
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2022-09-16 23:45:57 +0530

    function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,78 @@
-let a = 'Mango';
-    b = 'Guava';
-    c = 'Apple';
-    let i = prompt("Enter your favourite fruit")
+// let a = "Mango";
+// b = "Guava";
+// c = "Apple";
+// // let i = prompt("Enter your favourite fruit");
+
+// if (i < 5) {
+// 	console.log("Mango");
+// } else if (i <= 20);
+// console.log("Guava");
+
+// 1. Check if a number is odd or even in JavaScript
+
+// let num = 10;
+// num = 15;
+// num = 20;
+
+// num = prompt("Enter your nnumber")
+
+// function isEvenorOdd(num) {
+// 	if (num % 2 == 0) {
+// 		console.log(`${num} is a Even number`);
+// 	} else {
+// 		console.log(`${num} is a Odd number`);
+// 	}
+// }
+
+// isEvenorOdd(10); //"10 is a Even number"
+// isEvenorOdd(19); //"19 is a Odd number"
+
+// Hour
+// If hour is between 6am to 12pm: Good Morning!
+// If hour is between 12p, to 6pm: Good Afternoon!
+// Otherwise: Good Evening!
+
+// Objects
+
+let person = {
+    name: 'Ankita',
+    age: 15,
+    class: 'V'
+}
+
+let human = {
+    name: 'Chanchal',
+    age: 18,
+    address: 'House'
+}
+human.age = 30;
+console.log(human);
+
+let family = {
+    name: 'All family',
     
-    if (i < 5){
-        console.log("Mango");
-    }
-    else (i <= 20);
-        console.log("guwava");
-        elseif (i >= 20);
-        console.log("apple");
+}
+
+// Dot Notation
+person.name = 'Riya'
+console.log(person.name);
+
+// Array
+
+let selectedColors = ['red', 'blue', 'green'];
+selectedColors[3] = 100; // Add any extra object or number
+console.log(selectedColors);
+
+// Function
+
+function form(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName)
+}
+form('Riyansh', 'Yadav');
+
+// Calculating function
+
+function square(number){
+    return number * number;
+}
+console.log(square(2));
